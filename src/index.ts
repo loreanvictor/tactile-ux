a4ed26520cf2beb78c6bfbe00aa26a427b2eeb62
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2021-01-26 21:43:03 +0100

    fix some exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export { GlobalStyles } from './global';
+export { Fonts } from './fonts';
 export { Loading } from './loading';
 export { Button } from './button';
 export { DarkModeToggle } from './darkmode-toggle';
+export { TextInput } from './text-input';
